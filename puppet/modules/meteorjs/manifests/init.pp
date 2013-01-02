0f92fcc87cacc039aeb804c4ced90a7bfd970f56
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2013-01-02 17:49:25 +0000

    added nginx to proxy meteor.example.com to localhost:3000 running on the devbox. The allows you to acces the default project setup outof the box

--- puppet/modules/meteorjs/manifests/init.pp
+++ puppet/modules/meteorjs/manifests/init.pp
@@ -9,10 +9,37 @@
 
 class meteorjs {
 
+    include nginx
+
     exec {
         "meteor-curl-install":
             command => "su -c 'curl https://install.meteor.com | /bin/sh'",
             path => "/bin:/usr/bin:/usr/local/bin",
+            # If this is present it won't rerun the web based install:
+            creates => "/usr/bin/meteor"
     }
+
+    # Proxy the default localhost:3000 meteor port. Connecting to the hostname
+    # meteor.example.com -> localhost:3000
+    nginx::resource::upstream {'meteor-proxy':
+        ensure  => present,
+        members => [
+            'localhost:3000',
+        ],
+    }
+    nginx::resource::vhost {"meteor.example.com":
+        ensure => present,
+        proxy => "http://meteor-proxy",
+    }
+    exec {
+        "reload-nginx":
+            command => "sudo /etc/init.d/nginx reload",
+            path => "/bin:/usr/bin:/usr/local/bin",
+    }
+
+    Exec["meteor-curl-install"] ->
+    Nginx::Resource::Upstream['meteor-proxy'] ->
+    Nginx::Resource::Vhost['meteor.example.com'] ->
+    Exec["reload-nginx"]
 
 }
